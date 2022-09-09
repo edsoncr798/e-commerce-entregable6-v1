964ea12a1a23dc130ffde7704a713d1eec3e52fe
edson andi <edsoncr798@gmail.com>
2022-09-09 00:16:28 -0500

    arreglo error de ruta ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home  from './components/routes/Home'
 import ProductDetail from "./components/routes/ProductDetail"
 import Login from "./components/routes/Login"
 import Purchases from "./components/routes/Purchases"
-import Header from "./components/shared/header"
+import Header from "./components/shared/Header"
 import { HashRouter } from 'react-router-dom'
 import './App.css'
 
